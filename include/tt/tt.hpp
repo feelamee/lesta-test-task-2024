44889d8d6e9eec5d0f279595b7f7bde8b1227e62
Hleb Yakavinich <fosterlounch@gmail.com>
2024-04-06 17:57:49 +0200

    add answer to first question

--- include/tt/tt.hpp
+++ include/tt/tt.hpp
@@ -5,6 +5,21 @@
 namespace tt
 {
 
+/*
+    the diff between my(1) impl and your(2):
+    ```
+       bool isEven(int value){return value%2==0;}
+    ```
+    is that 1 use feature of hardware - binary representaion,
+    it will not work on 3bit processor, haha)
+    and 2 use mathematical definition of even numbers
+
+    Theoreticaly, 1 will be faster.
+    But, in practice, compiler can optimize 2
+
+    Althrough, game programmers live in debug build most of time,
+    that's why this is important for game to be playable in debug.
+*/
 template <std::integral T>
 bool
 iseven(T const n)
