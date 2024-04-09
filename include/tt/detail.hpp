75f840dbde4222ce8705fe5a401b307c9d351e0e
Hleb Yakavinich <fosterlounch@gmail.com>
2024-04-09 02:14:55 +0200

    misc: code-style, return to relative paths in `build/toolchain/BUILD.gn`
    Includes also need relative paths.
    But GN provide `{{include_dirs}}` already with `-I` prefix.
    That's why to achieve absolute paths,
    I need too much 'bash wheel inventing'
    or just few minutes to re-read documentation of GN.
    So, I too lazy for this))

--- include/tt/detail.hpp
+++ include/tt/detail.hpp
@@ -5,7 +5,17 @@
 namespace tt::detail
 {
 
+// with thanks stolen from https://github.com/hsutter/cppfront/
 template <typename T>
-using param_t = std::conditional_t<(sizeof(T) > sizeof(void*)), T&, T>;
+constexpr bool prefer_pass_by_value =
+    sizeof(T) <= 2 * sizeof(void*) && std::is_trivially_copy_constructible_v<T>;
+
+template <typename T>
+    requires std::is_class_v<T> || std::is_union_v<T> || std::is_array_v<T> || std::is_function_v<T>
+constexpr bool prefer_pass_by_value<T> = false;
+
+template <typename T>
+    requires(!std::is_void_v<T>)
+using param = std::conditional_t<prefer_pass_by_value<T>, T const, T const&>;
 
 } // namespace tt::detail
