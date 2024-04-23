f8a30ad46d3d0a4204af81cfb8555c57b4f1199e
Hleb Yakavinich <fosterlounch@gmail.com>
2024-04-23 23:05:59 +0200

    patch: add test for swap, append_range; fix bug in swap; upgrade to C++23 (Why not?)

--- tests/main.test.cpp
+++ tests/main.test.cpp
@@ -204,21 +204,30 @@ constexpr std::array tests = {
         buf1.clear();
         REQUIRE(buf1.empty());
         {
-            tt::ringbuf<int> buf2 = buf1;
+            tt::ringbuf<int> buf2{ buf1 };
             REQUIRE_EQ(buf2.capacity(), 1)
 
             buf2.emplace_back(5);
             REQUIRE_NEQ(buf1, buf2);
         }
         {
-            auto buf2 = buf1;
+            auto buf2{ buf1 };
             buf2.emplace_back(5);
             REQUIRE_EQ(*buf2.begin(), 5);
-            auto buf3 = buf2;
+            auto buf3{ buf2 };
             buf3.emplace_back(6);
             REQUIRE_EQ(*buf3.begin(), 6);
 
             REQUIRE_EQ(buf3.size(), buf2.size());
+        }
+        {
+            tt::ringbuf buf2{ buf1 };
+            buf2.emplace_back(5);
+
+            tt::ringbuf<int> buf3{ buf2.capacity() };
+            REQUIRE_NEQ(buf2, buf3);
+            buf3 = buf2;
+            REQUIRE_EQ(buf2, buf3);
         }
         return std::nullopt;
     },
@@ -256,6 +265,34 @@ constexpr std::array tests = {
         auto const v{ buf.pop_back() };
         REQUIRE(not v.has_value())
         REQUIRE_EQ(std::optional<int>(), v);
+        return std::nullopt;
+    },
+    +[]() -> std::optional<fail_info>
+    {
+        std::size_t const capacity1{ 5 };
+        tt::ringbuf<int> buf1{ capacity1 };
+
+        using std::views::all;
+        buf1.append_range(std::array{ 1, 2, 3, 4, 5, 6 } | all);
+        std::array expected{ 2, 3, 4, 5, 6 };
+        auto i{ buf1.begin() };
+        auto j{ expected.begin() };
+        for (; i != buf1.end() && j != expected.end(); ++i, ++j)
+            REQUIRE_EQ(*i, *j);
+
+        return std::nullopt;
+    },
+    +[]() -> std::optional<fail_info>
+    {
+        std::size_t const capacity1{ 5 };
+        tt::ringbuf<int> buf1{ capacity1 };
+
+        std::size_t const capacity2{ 3 };
+        tt::ringbuf<int> buf2{ capacity2 };
+
+        swap(buf1, buf2);
+        REQUIRE_EQ(capacity1, buf2.capacity());
+        REQUIRE_EQ(capacity2, buf1.capacity());
         return std::nullopt;
     },
 
